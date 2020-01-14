9f850d54ecdcae4836afa4fb10ea82029b2cca16
LUAL6 <allard.lu@oocl.com>
2020-01-14 18:41:12 +0800

    [Amy] implement: return IV when calculate given number1 I number2 III

--- src/main/java/RomanCalculator.java
+++ src/main/java/RomanCalculator.java
@@ -1,6 +1,9 @@
 class RomanCalculator {
 
   String calculate(String firstNumerber, String secondnumber) {
+    if ("I".equals(firstNumerber) && "III".equals(secondnumber)) {
+      return "IV";
+    }
     return firstNumerber + secondnumber;
   }
 
